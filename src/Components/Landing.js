f1ba60581262bf22cfe907f0f991ec61d5d218bc
Lichelle Gaerlan <lichellemaegaerlan@gmail.com>
2020-10-19 10:15:37 -0700

    recentered landing

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -12,36 +12,21 @@ export default props => {
         <div className="landing">
             <Container>
                 <Row>
-                    <div className="pink-pls">
-                        <Col xs={12} sm={12} md={12} lg={12}>
-                            {/* <div className="fade-in-about"><h1 id="about">Hi, My name is Lichelle</h1></div>
-                            <div className="fade-in-sub"><h2 id="about-sub">I'm a designer, developer, and computer networks specialist</h2></div> */}
-                            <img src={Landing} className="landing-image" />
-                        </Col>
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                        <img src={Landing} className="landing-image" />
+                    </Col>
 
-                        <Col xs={12} sm={12} md={12} lg={12}>
-                            <div className="home-link-container">
-                                <div className="fade-in-about">
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                        <div className="home-link-container">
+                            <div className="fade-in-about">
                                 <Link className="home-links" to="/about">About</Link>
                                 <Link className="home-links" to="/portfolio">Portfolio</Link>
                                 <Link className="home-links" to="/experience">Experience</Link>
                                 <Link className="home-links" to="/contact">Contact</Link>
-                                </div>
                             </div>
-                        </Col>
-                    </div>
-                </Row>
-
-                {/* <Row>
-                    <Col xs={12} sm={12} md={12} lg={12}>
-                        <div className="home-link-container">
-                            <Link className="home-links" to="/about">About</Link>
-                            <Link className="home-links" to="/experience">Experience</Link>
-                            <Link className="home-links" to="/portfolio">Portfolio</Link>
-                            <Link className="home-links" to="/contact">Contact</Link>
                         </div>
                     </Col>
-                </Row> */}
+                </Row>
             </Container>
         </div>
     );
