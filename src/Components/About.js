faa0b42dcc5e7a944f334a5a28f15501184c2bd3
Lichelle Gaerlan <lichellemaegaerlan@gmail.com>
2020-10-22 12:58:58 -0700

    about layout done (still need to add my process popup)

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,43 +1,89 @@
 import React from 'react';
 
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import Contact from './Contact.js';
 
 import './About.css';
-import AboutPic from './about.png';
+// import AboutPic from './about.png';
+import AboutPic from './about_lighter.png';
+import Devices from './devices.svg';
+import OS from './os.svg';
+import Other from './other.svg';
 
 export default props => {
     return(
-        <div className="about-pg">
-            <Container>
-                <Row>
-                    <Col xs={12} sm={12} md={4} lg={4}>
-                        <img src={AboutPic} className="about-polaroid" />
-                    </Col>
+        <div>
+            <div className="about-pg">
+                <Container>
+                    <Row>
+                        <Col xs={12} sm={12} md={4} lg={4} className="col-styles">
+                            <h1 className="about-headers">About</h1>
+                            <br/>
 
-                    <Col xs={12} sm={12} md={4} lg={4}>
-                        <h1>bio</h1>
-                    </Col>
+                            <img src={AboutPic} className="about-polaroid" />
+                            <br/><br/>
 
-                    <Col xs={12} sm={12} md={4} lg={4}>
-                        <h2>goals</h2>
-                    </Col>
-                </Row>
+                            {/* <p className="about-links-container">
+                                <a href="https://www.github.com/lichellegaerlan" className="about-links" target="_blank">Github</a> / <a href="https://www.linkedin.com/lichellemaegaerlan" className="about-links" target="_blank">LinkedIn</a> / <a className="about-links" href="mailto:lichellemaegaerlan@gmail.com">Mail</a>
+                            </p> */}
+                            {/* <button className="portfolio-button">My Portfolio</button> */}
+                            <br/>
+                        </Col>
 
-                {/* will probably need 2 rows worth of content l8r */}
-                {/* <Row>
-                    <Col xs={12} sm={12} md={4} lg={4}>
-                        <img src={AboutPic} className="about-polaroid" />
-                    </Col>
+                        <Col xs={12} sm={12} md={4} lg={4} className="col-styles" style={{ paddingRight: "20px"}}>
+                            <h1 className="about-headers">Bio</h1>
+                            <p className="about-text">
+                                My passion for technology and design began when I was a kid who was just
+                                creating HTML/CSS layouts on MySpace and Tumblr. <br/><br/>
 
-                    <Col xs={12} sm={12} md={4} lg={4}>
-                        <h1>bio</h1>
-                    </Col>
+                                Later on, I decided to pursue a degree in Computer Science from the University of Nevada, Las Vegas
+                                because I truly enjoyed spending my free-time creating beautiful, functional aesthetics and experiences during
+                                my early web development days. In my last semester of university, I took a Software Product Design course, where I 
+                                gained experience in conducting user research, creating prototypes, and improving my responsive frontend development skills. This course
+                                truly made me fall in love with combining design and code. <br/><br/>
 
-                    <Col xs={12} sm={12} md={4} lg={4}>
-                        <h2>goals</h2>
-                    </Col>
-                </Row> */}
-            </Container>
+                                As a designer, I love the process of creating meaningful experiences for users. I enjoy empathizing with
+                                users in order to pinpoint their problems and design creative solutions. I am passionate about how design and
+                                technology can bring about change in a vast majority of fields, like business, marketing, and education. <br/><br/>
+
+                                <button className="process-button">My Process</button>
+                                <button className="process-button"><Link to="/portfolio" className="process-link" target="_blank">My Portfolio</Link></button>
+                            </p>
+                        </Col>
+
+                        <Col xs={12} sm={12} md={4} lg={4} className="col-styles">
+                            <h1 className="about-headers">Personality</h1>
+                            <p className="about-text">
+                                <a href="https://www.16personalities.com/intj-personality" className="about-links" target="_blank"><b>The Architect (INTJ)</b></a> — 16Personalities <br/>
+                                "Imaginative and strategic thinkers, with a plan for everything.
+                                These thoughtful tacticians love perfecting the details of life, 
+                                applying creativity and rationality to everything they do."
+                            </p>
+                            <br/>
+
+                            <h2 className="about-headers">Goals</h2>
+                            <p className="about-text">
+                            <font color="#794f47">‣</font> To use my talent and creativity to create unique solutions <br/>
+                            <font color="#794f47">‣</font> To use my passion and drive for design, code, and customer service in useful ways <br/>
+                            <font color="#794f47">‣</font> To use my skills to contribute to a team, and to continually build my skills in turn
+                            </p>
+                            <br/>
+
+                            <h2 className="about-headers">Tech Usage</h2>
+                            <p className="about-text">
+                            <img src={Devices} height="11px" width="11px" /><span className="tab">Devices: Macbook Pro, iPad Pro, iPhone</span> <br/>
+                            <img src={OS} height="11px" width="11px" /><span className="tab">Operating Systems: Linux, Mac OS X, Windows</span> <br/>
+                            <img src={Other} height="11px" width="11px" /><span className="tab">Other: IT, Computer Networks</span> <br/>
+                            </p>
+                        </Col>
+                    </Row>
+
+                </Container>
+            </div>
+
+            <Contact />
         </div>
     );
 }
