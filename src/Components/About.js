d3a2d5af14be43a316460acb6392feaaba25fe62
Lichelle Gaerlan <lichellemaegaerlan@gmail.com>
2020-10-22 13:10:36 -0700

    updated about + dark themed contact footer

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-import Contact from './Contact.js';
+import Contact from './ContactDark.js';
 
 import './About.css';
 // import AboutPic from './about.png';
@@ -35,16 +35,16 @@ export default props => {
                         <Col xs={12} sm={12} md={4} lg={4} className="col-styles" style={{ paddingRight: "20px"}}>
                             <h1 className="about-headers">Bio</h1>
                             <p className="about-text">
-                                My passion for technology and design began when I was a kid who was just
-                                creating HTML/CSS layouts on MySpace and Tumblr. <br/><br/>
+                                My passion for technology and design began when I was a kid
+                                creating HTML/CSS layouts on MySpace and Tumblr for fun. <br/><br/>
 
-                                Later on, I decided to pursue a degree in Computer Science from the University of Nevada, Las Vegas
+                                I later decided to pursue a degree in Computer Science from the University of Nevada, Las Vegas
                                 because I truly enjoyed spending my free-time creating beautiful, functional aesthetics and experiences during
                                 my early web development days. In my last semester of university, I took a Software Product Design course, where I 
                                 gained experience in conducting user research, creating prototypes, and improving my responsive frontend development skills. This course
                                 truly made me fall in love with combining design and code. <br/><br/>
 
-                                As a designer, I love the process of creating meaningful experiences for users. I enjoy empathizing with
+                                I love the process of creating meaningful experiences for users. I enjoy empathizing with
                                 users in order to pinpoint their problems and design creative solutions. I am passionate about how design and
                                 technology can bring about change in a vast majority of fields, like business, marketing, and education. <br/><br/>
 
@@ -56,7 +56,7 @@ export default props => {
                         <Col xs={12} sm={12} md={4} lg={4} className="col-styles">
                             <h1 className="about-headers">Personality</h1>
                             <p className="about-text">
-                                <a href="https://www.16personalities.com/intj-personality" className="about-links" target="_blank"><b>The Architect (INTJ)</b></a> — 16Personalities <br/>
+                                <a href="https://www.16personalities.com/intj-personality" className="about-links" target="_blank"><b>The Architect (INTJ)</b></a> — Myer Briggs Type <br/>
                                 "Imaginative and strategic thinkers, with a plan for everything.
                                 These thoughtful tacticians love perfecting the details of life, 
                                 applying creativity and rationality to everything they do."
@@ -65,9 +65,9 @@ export default props => {
 
                             <h2 className="about-headers">Goals</h2>
                             <p className="about-text">
-                            <font color="#794f47">‣</font> To use my talent and creativity to create unique solutions <br/>
-                            <font color="#794f47">‣</font> To use my passion and drive for design, code, and customer service in useful ways <br/>
-                            <font color="#794f47">‣</font> To use my skills to contribute to a team, and to continually build my skills in turn
+                            <font color="#B48079">‣</font> To use my talent and creativity to create unique solutions <br/>
+                            <font color="#B48079">‣</font> To use my passion for designing, coding, and connecting with clients in useful ways <br/>
+                            <font color="#B48079">‣</font> To use my skills to contribute to a team, and to continually build my skills in turn
                             </p>
                             <br/>
 
